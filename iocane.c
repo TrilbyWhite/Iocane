dc8c0cd2ab76b7ec6e990a4dac584587c17eb592
Michael Kopp <kopp.michael@yahoo.de>
2015-09-15 11:30:39 +0200

    fix whitespace (spaces to tab), add vim modline

--- iocane.c
+++ iocane.c
@@ -4,18 +4,18 @@
 * Author: Jesse McClure, copyright 2012-2013
 * License: GPLv3
 *
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
 *
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
 *
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*	You should have received a copy of the GNU General Public License
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 \************************************************************************/
 
@@ -35,12 +35,12 @@ typedef struct { KeyCode key; char *command;} Key;
 
 /*! Operation mode of this program -- where to get commands from */
 typedef enum {
-    UNSET = 0, /*! no mode selected yet */
-    STDIN, /*! input from stdin (default) */
-    INTERACTIVE, /*! interpret key strokes */
-    INTERACTIVE_DISABLED, /*! Do not grab keys, only the key bound to toggle commands. */
-    COMMAND, /*! input from -c commands only, no other sources given */
-    INPUTFILE /*! input from file only */
+	UNSET = 0, /*! no mode selected yet */
+	STDIN, /*! input from stdin (default) */
+	INTERACTIVE, /*! interpret key strokes */
+	INTERACTIVE_DISABLED, /*! Do not grab keys, only the key bound to toggle commands. */
+	COMMAND, /*! input from -c commands only, no other sources given */
+	INPUTFILE /*! input from file only */
 } OperationMode;
 
 /* This will hold a table `key [keysymb] -> command [string]' of length `keycount' */
@@ -69,32 +69,29 @@ static const unsigned int mods_to_grab[] = {0,LockMask,Mod2Mask,LockMask|Mod2Mas
  */
 static void grab_keys()
 {
-    int i,j;
-    /* iterate over all elements in `keys' */
-    for (i = 0; i < keycount; i++)
-    {
-        for (j = 0; j < (sizeof(mods_to_grab)/sizeof(unsigned int)); j++)
-            XGrabKey(dpy, keys[i].key, mods_to_grab[j], root,True,GrabModeAsync, GrabModeAsync);
-    }
-    /* if defined and not already grabbed, grab the special key to grab/ungrab
-     * all keys */
-    if ((grab_ungrab_key != NO_TOGGLE_KEY) && (grab_ungrab_key_grabbed == False))
-    {
-        for (j = 0; j < (sizeof(mods_to_grab)/sizeof(unsigned int)); j++)
-            XGrabKey(dpy, grab_ungrab_key, mods_to_grab[j], root,True,GrabModeAsync, GrabModeAsync);
-        grab_ungrab_key_grabbed = True;
-    }
+	int i,j;
+	/* iterate over all elements in `keys' */
+	for (i = 0; i < keycount; i++) {
+		for (j = 0; j < (sizeof(mods_to_grab)/sizeof(unsigned int)); j++)
+			XGrabKey(dpy, keys[i].key, mods_to_grab[j], root,True,GrabModeAsync, GrabModeAsync);
+	}
+	/* if defined and not already grabbed, grab the special key to grab/ungrab
+	 * all keys */
+	if ((grab_ungrab_key != NO_TOGGLE_KEY) && (grab_ungrab_key_grabbed == False)) {
+		for (j = 0; j < (sizeof(mods_to_grab)/sizeof(unsigned int)); j++)
+			XGrabKey(dpy, grab_ungrab_key, mods_to_grab[j], root,True,GrabModeAsync, GrabModeAsync);
+		grab_ungrab_key_grabbed = True;
+	}
 }
 
 /* ungrab all keys */
 static void ungrab_keys()
 {
-    int i, j;
-    for (i = 0; i < keycount; i++)
-    {
-        for (j = 0; j < (sizeof(mods_to_grab)/sizeof(unsigned int)); j++)
-            XUngrabKey(dpy, keys[i].key, mods_to_grab[j], root);
-    }
+	int i, j;
+	for (i = 0; i < keycount; i++) {
+		for (j = 0; j < (sizeof(mods_to_grab)/sizeof(unsigned int)); j++)
+			XUngrabKey(dpy, keys[i].key, mods_to_grab[j], root);
+	}
 }
 
 /* Toggle whether to grab keys or not in interactive mode.
@@ -102,28 +99,27 @@ static void ungrab_keys()
  */
 static void toggle_interactive_mode()
 {
-    switch (mode)
-    {
-        case INTERACTIVE:
-            ungrab_keys();
-            printf("interactive mode off\n");
-            mode = INTERACTIVE_DISABLED;
-            break;
-        case INTERACTIVE_DISABLED:
-            grab_keys();
-            printf("interactive mode re-activated\n");
-            mode = INTERACTIVE;
-            break;
-        default:
-            fprintf(stderr, "Toggle command called in non-interactive mode.");
-            break;
-    }
+	switch (mode) {
+		case INTERACTIVE:
+			ungrab_keys();
+			printf("interactive mode off\n");
+			mode = INTERACTIVE_DISABLED;
+			break;
+		case INTERACTIVE_DISABLED:
+			grab_keys();
+			printf("interactive mode re-activated\n");
+			mode = INTERACTIVE;
+			break;
+		default:
+			fprintf(stderr, "Toggle command called in non-interactive mode.");
+			break;
+	}
 }
 
 /*! Send press-wait-release sequence to X for mouse button no `arg'.
  * Parts of this sequence can be disabled by passing `False' for doPress or
  * doRelease.
- * */
+ */
 static void press(int arg, Bool doPress, Bool doRelease) {
 	XEvent ev;
 	memset(&ev, 0x00, sizeof(ev));
@@ -131,7 +127,7 @@ static void press(int arg, Bool doPress, Bool doRelease) {
 	ev.type = ButtonPress;
 	ev.xbutton.button = arg;
 	ev.xbutton.same_screen = True;
-    /* fetch current coordinates of pointer; stored in ev */
+	/* fetch current coordinates of pointer; stored in ev */
 	XQueryPointer(dpy, root, &ev.xbutton.root, &ev.xbutton.window,
 			&ev.xbutton.x_root, &ev.xbutton.y_root, &ev.xbutton.x,
 			&ev.xbutton.y,&ev.xbutton.state);
@@ -142,41 +138,41 @@ static void press(int arg, Bool doPress, Bool doRelease) {
 				&ev.xbutton.subwindow, &ev.xbutton.x_root, &ev.xbutton.y_root,
 				&ev.xbutton.x, &ev.xbutton.y, &ev.xbutton.state);
 	}
-    if (doPress) {
-        /* send press command */
-        XSendEvent(dpy,PointerWindow,True,0xfff,&ev);
-        XFlush(dpy);
-        usleep(100000); /* wait for 0.1 s */
-    }
-    if (doRelease)
-    {
-        /* send release command */
-        ev.type = ButtonRelease;
-        ev.xbutton.state = 0x400;
-        XSendEvent(dpy,PointerWindow, True, 0xfff, &ev);
-        XFlush(dpy);
-    }
+	if (doPress) {
+		/* send press command */
+		XSendEvent(dpy,PointerWindow,True,0xfff,&ev);
+		XFlush(dpy);
+		usleep(100000); /* wait for 0.1 s */
+	}
+	if (doRelease)
+	{
+		/* send release command */
+		ev.type = ButtonRelease;
+		ev.xbutton.state = 0x400;
+		XSendEvent(dpy,PointerWindow, True, 0xfff, &ev);
+		XFlush(dpy);
+	}
 }
 
 
 /*! Interpret one line of `[<comand>] <x> <y>'; ignores empy lines and
  * comments. */
 static void command(char *line) {
-    /* ignore empty lines and comments */
+	/* ignore empty lines and comments */
 	if (line[0] == '\0' || line[0] == '\n' || line[0] == '#') return;
 	int x=0, y=0;
-    Bool isMove = False;
-    /* line begins with digit; parse "<number> <number>" to x,y */
+	Bool isMove = False;
+	/* line begins with digit; parse "<number> <number>" to x,y */
 	if (line[0] >= '0' && line[0] <= '9') {
-        sscanf(line,"%d %d",&x,&y);
-        isMove = True;
-    }
-    /* otherwise, ignore leading command and parse x,y */
+		sscanf(line,"%d %d",&x,&y);
+		isMove = True;
+	}
+	/* otherwise, ignore leading command and parse x,y */
 	else sscanf(line,"%*s %d %d",&x,&y);
 
-    /* interpret command (actually only first character is used),
-     * call appropriate X function */
-	if      (line[0] == 'p') XWarpPointer(dpy,None,root,0,0,0,0,sw,sh);
+	/* interpret command (actually only first character is used),
+	 * call appropriate X function */
+	if		(line[0] == 'p') XWarpPointer(dpy,None,root,0,0,0,0,sw,sh);
 	else if (line[0] == 'b') press(x, True, True); /* press and release */
 	else if (line[0] == 'h') press(x, True, False); /* press without release */
 	else if (line[0] == 'r') press(x, False, True); /* release without press */
@@ -187,45 +183,45 @@ static void command(char *line) {
 	else if (line[0] == 'e') running = False;
 	else if (line[0] == 's') { sleep(x); usleep(y*1000); }
 	else if (isMove == True) XWarpPointer(dpy,None,root,0,0,0,0,x,y);
-    else fprintf(stderr, "unable to interpret command '%s'", line);
+	else fprintf(stderr, "unable to interpret command '%s'", line);
 	XFlush(dpy);
 }
 
 int main(int argc, const char **argv) {
 	int i = 0;
 	FILE *input = NULL;
-    /* Try to connect to X server */
+	/* Try to connect to X server */
 	if (!(dpy=XOpenDisplay(0x0))) return 1;
-    /* Initialize globals */
+	/* Initialize globals */
 	scr = DefaultScreen(dpy);
 	root = RootWindow(dpy,scr);
 	sw = DisplayWidth(dpy,scr);
 	sh = DisplayHeight(dpy,scr);
-    /* parse command line arguments */
+	/* parse command line arguments */
 	for (i = 1; i < argc; i++) {
 		if (argv[i][0] == '-') {
 			if (argv[i][1] == 'h') { printf(HELP); exit(0); }
 			else if (argv[i][1] == 'i') mode = INTERACTIVE;
 			else if (argv[i][1] == 's') mode = STDIN;
 			else if (argv[i][1] == 'c' && argc > i+1) {
-                // interpret the content of the `-c' argument
+				// interpret the content of the `-c' argument
 				command((char *)argv[(++i)]);
 				if (mode == UNSET) mode = COMMAND;
 			}
 		}
 		else {
-            /* file input; try to open the file */
+			/* file input; try to open the file */
 			printf("in = ",argv[i]);
 			input = fopen(argv[i],"r");
 			if (input) mode = INPUTFILE;
 		}
 	}
 
-    /* if input was only -c parameters, done */
+	/* if input was only -c parameters, done */
 	if (mode == COMMAND) exit(0);
-    /* if no input file was opened, use stdin */
+	/* if no input file was opened, use stdin */
 	if (!input) input = stdin;
-    /* iterate over given list of commands */
+	/* iterate over given list of commands */
 	if (mode == UNSET || mode == STDIN || mode == INPUTFILE) {
 		char line[MAXLINE+1];
 		while (running && (fgets(line,MAXLINE,input)) != NULL)
@@ -238,7 +234,7 @@ int main(int argc, const char **argv) {
 
 	/* no args -> interactive mode: */
 
-    /* search for config file */
+	/* search for config file */
 	char *dir = getenv("PWD");
 	chdir(getenv("XDG_CONFIG_HOME"));
 	chdir("iocane");
@@ -249,68 +245,70 @@ int main(int argc, const char **argv) {
 	}
 	if (!rcfile) rcfile = fopen("/usr/share/iocane/iocanerc","r");
 	if (dir) chdir(dir); /* go back to current directory */
-    /* if unable to find a config file, quit */
+	/* if unable to find a config file, quit */
 	if (!rcfile) {
 		fprintf(stderr,"IOCANE: no iocanerc file found.\n");
 		XCloseDisplay(dpy);
 		return 0;
 	}
 
-    /* parse config file */
+	/* parse config file */
 	KeySym keysym;
 	char *line = (char *) calloc(MAXLINE+MAXSYMLEN+2,sizeof(char));
-    char *command_string;
+	char *command_string;
 	char keystring[MAXSYMLEN];
 	keycount = 0;
 	while (fgets(line,MAXLINE+MAXSYMLEN+2,rcfile) != NULL) {
 		if (line[0] == '#' || line[0] == '\n') continue;
-        /* split each line at space, parse into keysym,command_string */
+		/* split each line at space, parse into keysym,command_string */
 		strncpy(keystring,line,MAXSYMLEN); *strchr(keystring,' ') = '\0';
 		if ( (keysym=XStringToKeysym(keystring)) == NoSymbol ) continue;
 		command_string = (char *) calloc(strlen(line) - strlen(keystring), sizeof(char));
 		strcpy(command_string,strchr(line,' ')+1);
-        /* check, if it's the toggle command */
-        if (command_string[0] == 't') {
-            /* if so, set the special toggle key */
-            grab_ungrab_key = XKeysymToKeycode(dpy,keysym);
-        }
-        else {
-            /* otherwise (normal key) add keysymbol and command to keys */
-            keys = realloc(keys, (keycount+1) * sizeof(Key));
-            keys[keycount].key = XKeysymToKeycode(dpy,keysym);
-            keys[keycount].command = command_string;
-            keycount++;
-        }
+		/* check, if it's the toggle command */
+		if (command_string[0] == 't') {
+			/* if so, set the special toggle key */
+			grab_ungrab_key = XKeysymToKeycode(dpy,keysym);
+		}
+		else {
+			/* otherwise (normal key) add keysymbol and command to keys */
+			keys = realloc(keys, (keycount+1) * sizeof(Key));
+			keys[keycount].key = XKeysymToKeycode(dpy,keysym);
+			keys[keycount].command = command_string;
+			keycount++;
+		}
 	}
-    /* done parsing; clean up */
+	/* done parsing; clean up */
 	free(line);
 	fclose(rcfile);
 
-    /* actually grab the keys */
-    grab_keys();
+	/* actually grab the keys */
+	grab_keys();
 
-    /* loop for interactive mode
-     * Wait for event (blocks) of a grabbed key.  If the key is registered in
-     * the `keys' list, then the associated command is executed.
-     */
+	/* loop for interactive mode
+	 * Wait for event (blocks) of a grabbed key.  If the key is registered in
+	 * the `keys' list, then the associated command is executed.
+	 */
 	XEvent ev;
 	XKeyEvent *e;
 	while (running && !XNextEvent(dpy,&ev)) if (ev.type == KeyPress) {
 		e = &ev.xkey;
-        if (e->keycode == grab_ungrab_key) {
-            toggle_interactive_mode();
-        }
-        else {
-            for (i = 0; i < keycount; i++)
-                if (e->keycode == keys[i].key && keys[i].command)
-                    command(keys[i].command); /* execute associated command */
-        }
+		if (e->keycode == grab_ungrab_key) {
+			toggle_interactive_mode();
+		}
+		else {
+			for (i = 0; i < keycount; i++)
+				if (e->keycode == keys[i].key && keys[i].command)
+					command(keys[i].command); /* execute associated command */
+		}
 	}
 
-    /* program done; cleanup */
+	/* program done; cleanup */
 	for (i = 0; i < keycount; i++) free(keys[i].command);
 	free(keys);
 	XCloseDisplay(dpy); /* close connection to X server and release resources */
 	return 0;
 }
 
+
+/* vim: set tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab : */
